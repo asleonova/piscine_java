21145138fd3df5af87f6e93abcaf50ab8dc26104
Anna Leonova <asleonova.1@gmail.com>
2021-05-30 20:39:16 +0300

    lala

---
+++ ex01/Program.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Program {
+
+    int _num;
+
+    public Program(int num) {
+        this._num = num;
+    }
+
+    public static void main(String args[])
+    {
+        for (String s: args) {
+            System.out.println(s);
+    }
+}
+}
